a9273dc645034d0bca227ca9d0f4b45ed59c7875
hayversong <hayversoon@gmail.com>
2026-05-19 20:09:56 -0300

    feat: adicionando armazenamento melhorado

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -27,6 +29,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 					Titulo: tarefa,
 				},
 			)
+			salvarTarefas()
 		}
 
 		http.Redirect(
@@ -69,6 +72,7 @@ func concluir(w http.ResponseWriter, r *http.Request) {
 
 		tarefas[id].Concluida =
 			!tarefas[id].Concluida
+		salvarTarefas()
 	}
 
 	http.Redirect(
@@ -93,6 +97,7 @@ func excluir(w http.ResponseWriter, r *http.Request) {
 				tarefas[:id],
 				tarefas[id+1:]...,
 			)
+		salvarTarefas()
 	}
 
 	http.Redirect(
@@ -103,7 +108,51 @@ func excluir(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
+func salvarTarefas() {
+
+	arquivo, err :=
+		os.Create(
+			"tarefas.json",
+		)
+
+	if err != nil {
+
+		return
+	}
+
+	defer arquivo.Close()
+
+	json.NewEncoder(
+		arquivo,
+	).Encode(
+		tarefas,
+	)
+}
+
+func carregarTarefas() {
+
+	arquivo, err :=
+		os.Open(
+			"tarefas.json",
+		)
+
+	if err != nil {
+
+		return
+	}
+
+	defer arquivo.Close()
+
+	json.NewDecoder(
+		arquivo,
+	).Decode(
+		&tarefas,
+	)
+}
+
 func main() {
+
+	carregarTarefas()
 
 	http.HandleFunc("/", home)
 
