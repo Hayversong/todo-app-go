26af0871e7e58c63eab199f199c723f447466c9a
hayversong <hayversoon@gmail.com>
2026-05-19 14:54:08 -0300

    criando to-do app com go

---
+++ README.md
@@ -0,0 +1,10 @@
+# To-Do App em Go
+
+Aplicação web feita em Go para estudar:
+
+- HTTP
+- HTML Templates
+- Formulários
+- Structs
+- Rotas
+- CRUD
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type Tarefa struct {
+	Titulo    string
+	Concluida bool
+}
+
+var tarefas []Tarefa
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		tarefa := r.FormValue("tarefa")
+
+		if tarefa != "" {
+
+			tarefas = append(
+				tarefas,
+				Tarefa{
+					Titulo: tarefa,
+				},
+			)
+		}
+
+		http.Redirect(
+			w,
+			r,
+			"/",
+			http.StatusSeeOther,
+		)
+
+		return
+	}
+
+	tmpl, err := template.ParseFiles(
+		"static/index.html",
+	)
+
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusInternalServerError,
+		)
+
+		return
+	}
+
+	tmpl.Execute(
+		w,
+		tarefas,
+	)
+}
+
+func concluir(w http.ResponseWriter, r *http.Request) {
+
+	id, _ := strconv.Atoi(
+		r.URL.Query().Get("id"),
+	)
+
+	if id >= 0 && id < len(tarefas) {
+
+		tarefas[id].Concluida =
+			!tarefas[id].Concluida
+	}
+
+	http.Redirect(
+		w,
+		r,
+		"/",
+		http.StatusSeeOther,
+	)
+}
+
+func main() {
+
+	http.HandleFunc("/", home)
+
+	http.HandleFunc(
+		"/concluir",
+		concluir,
+	)
+
+	http.ListenAndServe(
+		":8080",
+		nil,
+	)
+}
